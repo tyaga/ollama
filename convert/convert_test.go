e77bbcee364820515970b962f35bb1e989768b74
Josh Yan <jyan00017@gmail.com>
2024-07-26 17:48:15 -0700

    cmdr conv

--- convert/convert_test.go
+++ convert/convert_test.go
@@ -27,10 +27,12 @@ func convertFull(t *testing.T, fsys fs.FS) (*os.File, llm.KV, llm.Tensors) {
 	}
 	defer f.Close()
 
+	fmt.Println("converting")
 	if err := Convert(fsys, f); err != nil {
 		t.Fatal(err)
 	}
 
+	fmt.Println("convert succeeded")
 	r, err := os.Open(f.Name())
 	if err != nil {
 		t.Fatal(err)
@@ -67,6 +69,7 @@ func TestConvertFull(t *testing.T) {
 		"Phi-3-mini-128k-instruct",
 		"all-MiniLM-L6-v2",
 		"gemma-2-9b-it",
+		"c4ai-command-r-v01",
 	}
 
 	for i := range cases {
@@ -81,6 +84,7 @@ func TestConvertFull(t *testing.T) {
 				t.Skipf("%s not found", p)
 			}
 
+			fmt.Println("Converting", tt)
 			f, kv, tensors := convertFull(t, os.DirFS(p))
 			actual := make(map[string]string)
 			for k, v := range kv {
